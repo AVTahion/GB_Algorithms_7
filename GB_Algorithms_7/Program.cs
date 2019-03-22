f31b2a6a600ecfbf0ce38dc3c664f3970907cb56
AVTahion <avkontakt40@gmail.com>
2019-03-22 15:23:10 +0300

    Task_1 добавлена замена обозначений +бесконечности на "." при выводе матрицы в консоль. Task_2 Метод обхода графа в ширину

--- GB_Algorithms_7/Program.cs
+++ GB_Algorithms_7/Program.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-/*  2.  Написать функции, которые считывают матрицу смежности из файла и выводят её на экран.
-        Написать рекурсивную функцию обхода графа в глубину.
+/*  1.  Написать функции, которые считывают матрицу смежности из файла и выводят её на экран.
+    2.  Написать рекурсивную функцию обхода графа в глубину.
         Написать функцию обхода графа в ширину.
     3.  *Создать библиотеку функций для работы с графами.
 
@@ -19,8 +19,11 @@ namespace GB_Algorithms_7
         /// <summary>
         /// Метод считывает матрицу и выводит ее в консоль
         /// </summary>
-        /// <param name="pathToFile"></param>
-        static int[,] ReadAndPrint (string pathToFile)
+        /// <param name="pathToFile">Путь к файлу с матрицей смежности</param>
+        /// <param name="infinity">Обозначение +бесконечности в матрице(по умолчанию = 2 для невзвешанного графа),
+        /// при выводе в консоль заменяетя "." для улучшения читаемости</param>
+        /// <returns></returns>
+        static int[,] ReadAndPrint (string pathToFile, int infinity = 2)
         {
             int[,] testInt = new int[2,2];
             try
@@ -51,7 +54,11 @@ namespace GB_Algorithms_7
                 {
                     for (int j = 0; j < testInt.GetLength(1); j++)
                     {
-                        Console.Write($"{testInt[i, j]}\t");
+                        if (testInt[i, j] == infinity)
+                        {
+                            Console.Write($".\t");
+                        }
+                        else Console.Write($"{testInt[i, j]}\t");
                     }
                     Console.WriteLine();
                 }
@@ -63,10 +70,55 @@ namespace GB_Algorithms_7
             return testInt;
         }
 
+        /// <summary>
+        /// Метод обхода неориентированного графа(взвешенного/не взвешенного) в ширину
+        /// </summary>
+        /// <param name="matrix">Матрица смежности</param>
+        /// <param name="marker">Стартовая вершина(индекс матрицы)</param>
+        /// <param name="infinity">Обозначение +бесконечности в матрице(по умолчанию = 2 для невзвешанного графа)</param>
+        /// <returns>Массив окрашенных вершин графа</returns>
+        static int[] GraphToWide(int[,] matrix, int marker, int infinity = 2)
+        {
+            int[] resultArr = new int[matrix.GetLength(0)];
+            Queue<int> queue = new Queue<int>();
+            int colore = 2;
+            queue.Enqueue(marker); // добавляем стартовую вершину в обработку
+            resultArr[marker] = 1; // помечаем стартовую как обнаруженную
+            W: while (queue.Count != 0)
+            {
+                int checkingEl = queue.Dequeue();
+                for (int i = 0; i < matrix.GetLength(1); i++)
+                {
+                    if (infinity > matrix[checkingEl, i] && matrix[checkingEl, i] > 0 && resultArr[i] == 0)
+                    {   // вершина обнаружена и добавлена в обработку
+                        queue.Enqueue(i);
+                        resultArr[i] = 1; 
+                    }
+                }
+                resultArr[checkingEl] = colore; // вершина обработана
+            }
+            for (int i = 0; i < resultArr.Length; i++) // проверяем наличие несвязанных компонентов графа
+            {
+                if (resultArr[i] == 0)
+                {
+                    colore++;
+                    queue.Enqueue(i);
+                    goto W;
+                }
+            }
+            return resultArr;
+        }
+
+
         static void Main(string[] args)
         {
-            int[,] arr = ReadAndPrint(@"..\..\123.txt");
+            int[,] arr = ReadAndPrint(@"..\..\123.txt", 9);
             Console.WriteLine();
+            int[] res = GraphToWide(arr, 2, 9);
+            foreach (var item in res)
+            {
+                Console.Write(item + " ");
+            }
             Console.ReadKey();
         }
     }
